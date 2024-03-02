75de3e8fd7ae85600e77c66068cf5fcc77167f0e
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-03-02 20:52:58 +0100

    Implement Part 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+import "../styles.css";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState();
+
+  const toggleView = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className="blog">
+      {blog.title} <button onClick={toggleView}>view</button>
+      {visible && (
+        <>
+          <p>Author: {blog.author}</p>
+          {blog.url && <p>URL: {blog.url}</p>}
+          {parseInt(blog.likes) >= 0 && <p>Likes: {blog.likes}</p>}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
